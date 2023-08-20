8d2a620000517aae89dd300d2ac2b017305f1e01
anscaryo <anscaryo@gmail.com>
2023-08-20 12:09:55 +0200

    Añado el resto de archivos del curso 1, y los realizados en el curso 2

--- Tkinter/ventana_4.py
+++ Tkinter/ventana_4.py
@@ -3,19 +3,20 @@ import tkinter as tk
 
 # def pasaTexto():
 #     mensaje = cajaTexto.get()
-#     print(mensaje)
+#     print(mensaje)∫
 #     etiqueta["text"] = mensaje
 
 
 ventana = tk.Tk()
 ventana.geometry("300x250")
 ventana.title("Cuarta Ventana")
+ventana.iconbitmap("graficos/pin_icon.ico")
 
-boton1 = tk.Button(ventana, text="Boton1", width=5, height=5)
+boton1 = tk.Button(ventana, text="Boton1", width=2, height=1)
 
-boton2 = tk.Button(ventana, text="Boton2", width=5, height=5)
+boton2 = tk.Button(ventana, text="Boton2", width=2, height=1)
 
-boton3 = tk.Button(ventana, text="Boton3", width=5, height=5)
+boton3 = tk.Button(ventana, text="Boton3", width=2, height=1)
 
 boton1.grid(row=0, column=0)
 boton2.grid(row=1, column=1)
